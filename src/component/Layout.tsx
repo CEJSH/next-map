b465e9d8373ac12dc38bafd77dbc2821bb2e55d6
CEJSH <eun2763@naver.com>
2024-09-03 00:42:14 +0900

    tailwind

---
+++ src/component/Layout.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+import Navbar from "./Navbar";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="layout">
+      <Navbar />
+      {children}
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1>Map Index Page</h1>
-      <ul>
+      <h1 className="font-bold">Map Index Page</h1>
+      <ul className="px-4 py-2">
         <li>
           <Link href="/stores">맛집 목록</Link>
         </li>
