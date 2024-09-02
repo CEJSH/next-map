d49c480cb209f875e495f473e3e91b6dd02af861
CEJSH <eun2763@naver.com>
2024-09-02 20:16:58 +0900

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import "@/styles/globals.css";
+// import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-
+// 모든 페이지가 거쳐가는 파일
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
