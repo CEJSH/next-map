4e10f307ba6226eb6c8b4e26f5d2ede800307e1c
CEJSH <eun2763@naver.com>
2024-09-03 13:03:03 +0900

    Add map

---
+++ src/components/Map.tsx
@@ -0,0 +1,47 @@
+/** global kakao */
+import Script from "next/script";
+import * as stores from "@/data/store_data.json";
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+const DEFAULT_LAT = 37.479625203;
+const DEFAULT_LNG = 127.03088379;
+
+export default function Map() {
+  const loadKakaoMap = () => {
+    window.kakao.maps.load(() => {
+      const mapContainer = document.getElementById("map");
+      const mapOption = {
+        center: new window.kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG),
+        level: 3,
+      };
+      const map = new window.kakao.maps.Map(mapContainer, mapOption);
+
+      // 실당 데이터 마커 띄우기
+      stores?.["DATA"]?.map((store) => {
+        var markerPosition = new window.kakao.maps.LatLng(
+          store?.y_dnts,
+          store?.x_cnts
+        );
+
+        var marker = new window.kakao.maps.Marker({ position: markerPosition });
+
+        marker.setMap(map);
+      });
+    });
+  };
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        type="text/javascript"
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_CLIENT}&autoload=false`}
+        onReady={loadKakaoMap}
+      />
+      <div id="map" className="w-full h-screen"></div>
+    </>
+  );
+}
