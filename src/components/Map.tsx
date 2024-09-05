268257f4858d7d72ea4aae77edfc11b3bb384d95
CEJSH <eun2763@naver.com>
2024-09-06 03:52:12 +0900

    store detail marker & refetchOnWindowFocus

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -10,17 +10,24 @@ declare global {
 const DEFAULT_LAT = 37.479625203;
 const DEFAULT_LNG = 127.03088379;
 
+const DEFAULT_ZOOM = 3;
 interface MapProps {
   setMap: Dispatch<SetStateAction<any>>;
+  lat?: string | null;
+  lng?: string | null;
+  zoom?: number;
 }
 
-export default function Map({ setMap }: MapProps) {
+export default function Map({ setMap, lat, lng, zoom }: MapProps) {
   const loadKakaoMap = () => {
     window.kakao.maps.load(() => {
       const mapContainer = document.getElementById("map");
       const mapOption = {
-        center: new window.kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG),
-        level: 3,
+        center: new window.kakao.maps.LatLng(
+          lat ?? DEFAULT_LAT,
+          lng ?? DEFAULT_LNG
+        ),
+        level: zoom ?? 3,
       };
       const map = new window.kakao.maps.Map(mapContainer, mapOption);
 
